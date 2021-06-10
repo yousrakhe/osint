163636ca9e8de1e045e38cbc98d5218903fdd3c9
yousrakhe <85696165+yousrakhe@users.noreply.github.com>
2021-06-10 17:57:16 +0200

    Create osint.py

---
+++ osint.py
@@ -0,0 +1,6 @@
+from metagoofil import *
+
+
+
+mg=Metagoofil('usthb.dz',30,False,15,100,100,'usthb',8,['pdf'],'Mozilla/5.0 (compatible; Googlebot/2.1; +http://www.google.com/bot.html)',False)
+mg.go()
